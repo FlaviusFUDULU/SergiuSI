4394bbb9ee3a10fc3fce82e4963ee5bbf8c6a94d
Flavius Fudulu <flavius.fudulu@theaccessgroup.com>
2020-05-26 23:27:45 +0300

    Initial Commit

---
+++ Problema2/Problema2/Program.cs
@@ -0,0 +1,88 @@
+﻿using RSATest;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Problema2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = ".\\Resources\\TextInitial.txt";
+            string encryptedTextFilePath = ".\\Resources\\OprisSergiu_crypt.txt";
+            string decryptedTextFilePath = ".\\Resources\\OprisSergiu_decrypt.txt";
+            string timpTextFilePath = ".\\Resources\\OprisSergiu_timp.txt";
+
+            string initialFileText = GetTextFromFile(filePath);
+
+            string privateKey =
+                "MIIEpAIBAAKCAQEAoQh0wEqx/R2H1v00IU12Oc30fosRC/frhH89L6G+fzeaqI19MYQhEPMU13wpeqRONCUta+2iC1sgCNQ9qGGf19yGdZUfueaB1Nu9rdueQKXgVurGHJ+5N71UFm+OP1XcnFUCK4wT5d7ZIifXxuqLehP9Ts6sNjhVfa+yU+VjF5HoIe69OJEPo7OxRZcRTe17khc93Ic+PfyqswQJJlY/bgpcLJQnM+QuHmxNtF7/FpAx9YEQsShsGpVo7JaKgLo+s6AFoJ4QldQKir2vbN9vcKRbG3piElPilWDpjXQkOJZhUloh/jd7QrKFimZFldJ1r6Q59QYUyGKZARUe0KZpMQIDAQABAoIBAQCRZLUlOUvjIVqYvhznRK1OG6p45s8JY1r+UnPIId2Bt46oSLeUkZvZVeCnfq9k0Bzb8AVGwVPhtPEDh73z3dEYcT/lwjLXAkyPB6gG5ZfI/vvC/k7JYV01+neFmktw2/FIJWjEMMF2dvLNZ/Pm4bX1Dz9SfD/45Hwr8wqrvRzvFZsj5qqOxv9RPAudOYwCwZskKp/GF+L+3Ycod1Wu98imzMZUH+L5dQuDGg3kvf3ljIAegTPoqYBg0imNPYY/EGoFKnbxlK5S5/5uAFb16dGJqAz3XQCz9Is/IWrOTu0etteqV2Ncs8uqPdjed+b0j8CMsr4U1xjwPQ8WwdaJtTkRAoGBANAndgiGZkCVcc9975/AYdgFp35W6D+hGQAZlL6DmnucUFdXbWa/x2rTSEXlkvgk9X/PxOptUYsLJkzysTgfDywZwuIXLm9B3oNmv3bVgPXsgDsvDfaHYCgz0nHK6NSrX2AeX3yO/dFuoZsuk+J+UyRigMqYj0wjmxUlqj183hinAoGBAMYMOBgF77OXRII7GAuEut/nBeh2sBrgyzR7FmJMs5kvRh6Ck8wp3ysgMvX4lxh1ep8iCw1R2cguqNATr1klOdsCTOE9RrhuvOp3JrYzuIAK6MpH/uBICy4w1rW2+gQySsHcH40r+tNaTFQ7dQ1tef//iy/IW8v8i0t+csztE1JnAoGABdtWYt8FOYP688+jUmdjWWSvVcq0NjYeMfaGTOX/DsNTL2HyXhW/Uq4nNnBDNmAz2CjMbZwt0y+5ICkj+2REVQVUinAEinTcAe5+LKXNPx4sbX3hcrJUbk0m+rSu4G0B/f5cyXBsi9wFCAzDdHgBduCepxSr04Sc9Hde1uQQi7kCgYB0U20HP0Vh+TG2RLuE2HtjVDD2L/CUeQEiXEHzjxXWnhvTg+MIAnggvpLwQwmMxkQ2ACr5sd/3YuCpB0bxV5o594nsqq9FWVYBaecFEjAGlWHSnqMoXWijwu/6X/VOTbP3VjH6G6ECT4GR4DKKpokIQrMgZ9DzaezvdOA9WesFdQKBgQCWfeOQTitRJ0NZACFUn3Fs3Rvgc9eN9YSWj4RtqkmGPMPvguWo+SKhlk3IbYjrRBc5WVOdoX8JXb2/+nAGhPCuUZckWVmZe5pMSr4EkNQdYeY8kOXGSjoTOUH34ZdKeS+e399BkBWIiXUejX/Srln0H4KoHnTWgxwNpTsBCgXu8Q==";
+
+            string publicKey =
+                "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoQh0wEqx/R2H1v00IU12Oc30fosRC/frhH89L6G+fzeaqI19MYQhEPMU13wpeqRONCUta+2iC1sgCNQ9qGGf19yGdZUfueaB1Nu9rdueQKXgVurGHJ+5N71UFm+OP1XcnFUCK4wT5d7ZIifXxuqLehP9Ts6sNjhVfa+yU+VjF5HoIe69OJEPo7OxRZcRTe17khc93Ic+PfyqswQJJlY/bgpcLJQnM+QuHmxNtF7/FpAx9YEQsShsGpVo7JaKgLo+s6AFoJ4QldQKir2vbN9vcKRbG3piElPilWDpjXQkOJZhUloh/jd7QrKFimZFldJ1r6Q59QYUyGKZARUe0KZpMQIDAQAB";
+                //GetNewRSAPublicKey();
+
+            var rsaProvider = new RSAProvider(RSAType.RSA2, Encoding.UTF8, privateKey, publicKey);
+
+            Stopwatch stopWatchencrypt = Stopwatch.StartNew();
+
+            string encryptedText = rsaProvider.Encrypt(initialFileText);
+
+            stopWatchencrypt.Stop();
+
+            WriteTextToFIle(encryptedText, encryptedTextFilePath);
+
+            Stopwatch stopWatchDecrypt = Stopwatch.StartNew();
+
+            string descryptedText = rsaProvider.Decrypt(encryptedText);
+
+            stopWatchDecrypt.Stop();
+
+            WriteTextToFIle(descryptedText, decryptedTextFilePath);
+
+            string timeTextFile = $"Encryption time: {stopWatchencrypt.ElapsedMilliseconds} ms \r\n" +
+                $"Descryption time: {stopWatchDecrypt.ElapsedMilliseconds} ms";
+            WriteTextToFIle(timeTextFile, timpTextFilePath);
+        }
+
+        private static string GetNewRSAPublicKey()
+        {
+            RSA rsa = RSA.Create();
+            return Convert.ToBase64String(rsa.ExportRSAPublicKey());
+        }
+
+        private static void WriteTextToFIle(string text, string filePath)
+        {
+            // Write the string array to a new file named "WriteLines.txt".
+            using (StreamWriter outputFile = new StreamWriter(filePath))
+            {
+                foreach (string line in text.Split("\r\n"))
+                    outputFile.WriteLine(line);
+            }
+        }
+
+        private static string GetTextFromFile(string filePath)
+        {
+            try
+            {   // Open the text file using a stream reader.
+                string text = null;
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    String line = sr.ReadToEnd();
+                    text = text + line;
+                }
+                return text;
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
